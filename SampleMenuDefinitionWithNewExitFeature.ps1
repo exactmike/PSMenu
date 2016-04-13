bacd95803d8592422fd0f8dbb1c77daf208e4111
exactmike <mike@exactsolutions.biz>
2016-04-12 23:25:13 -0400

    Add sample script with new Exit Feature demonstrated

---
+++ SampleMenuDefinitionWithNewExitFeature.ps1
@@ -0,0 +1,15 @@
+﻿Clear-Host
+Import-Module PSMenu -Force
+$menudefinition = [pscustomobject]@{
+                    GUID = '94301e0f-50f3-4af5-bee8-989753feb9a1'
+                    Title = 'Test'
+                    Initialization = $Null
+                    Choices = @(
+                        [pscustomobject]@{choice='this is choice 1';command='Write-Host -Object "This was Choice 1" -ForegroundColor Green';exit=$false}
+                        [pscustomobject]@{choice='this is choice 2 with Exit';command='Write-Host -Object "This was Choice 2" -ForegroundColor Red';exit=$true}                        
+                        [pscustomobject]@{choice='this is choice 3';command='Write-Host -Object "This was Choice 3" -ForegroundColor Yellow';exit=$false}                        
+                    )
+                    ParentGUID = $Null
+                }
+
+Invoke-Menu -MenuDefinition $menudefinition
